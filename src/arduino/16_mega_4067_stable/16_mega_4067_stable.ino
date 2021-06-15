167504d4ab697a4fcdbe3a03f8ce48526ea19e39
Atomie CHEN <atomic_cwh@163.com>
2021-06-15 16:40:59 +0800

    Major: alleviate the problem of ADC delay by multiplexing order

--- src/arduino/16_mega_4067_stable/16_mega_4067_stable.ino
+++ src/arduino/16_mega_4067_stable/16_mega_4067_stable.ino
@@ -7,11 +7,16 @@
 #define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit)) // clear bit in special funciton register
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit)) // set bit in special funciton register
 
-#define pin_mux_1_reg PORTB
-#define pin_mux_2_reg PORTL
+#define reg_x PORTB
+#define reg_y PORTL
+#define ang_in A0
 
-uint8_t pin_mux_1[4] = {50, 51, 52, 53};
-uint8_t pin_mux_2[4] = {46, 47, 48, 49};
+#define set_mux_reg(x, reg) (reg = (reg & 0xf0) + (x & 0xf))
+#define sample(out) (out = (analogRead(ang_in) >> 2))
+#define sample_delay(out, d) (delayMicroseconds(d),sample(out))
+
+uint8_t pin_mux_1[4] = {53, 52, 51, 50};
+uint8_t pin_mux_2[4] = {49, 48, 47, 46};
 uint32_t pin_out_vtg = A0;
 int buf[512];
 volatile byte out_voltage;
@@ -70,12 +75,6 @@ void set_ADC_interrupt() {
   sei();
 }
 
-// ISR(ADC_vect) {
-//   out_voltage = ADCH; // read 8 bit value from ADC
-//   cnt = 1;
-// //  Serial.println("#");
-// }
-
 void setup() {
   for (int i = 0; i < 4; i++) {
     pinMode(pin_mux_1[i], OUTPUT);
@@ -88,73 +87,85 @@ void setup() {
 }
 
 void loop() {
-//   scan();
-  // send();
+  scan();
+//  scan_delay();
+//  scan_opt();
 
-  scan_interrupt();
+//  scan_test_digital();
 }
 
-void scan_interrupt() {
+void scan() {
+  uint8_t x = 0, y = 0;
   Serial.write(0xff);
-  int ptr = 0;
-  for (int x = 0; x < 16; x++) {
-    for (int y = 0; y < 16; y++) {
-      // cnt = 0;
-      set_mux(x, y);
-      // while (cnt == 0);  // 这里需要等待ADC结果
-      int val = analogRead(A0);
-      out_voltage = val >> 2;
+  for (x = 0; x < 16; x++) {
+    set_mux_reg(x, reg_x);
+    for (y = 0; y < 16; y++) {
+      set_mux_reg(y, reg_y);
+      sample(out_voltage);
       Serial.write(out_voltage);
     }
   }
 }
 
-void scan() {
+void scan_delay() {
+  uint8_t delay = 50;
+  uint8_t x = 0, y = 0;
   Serial.write(0xff);
-  int ptr = 0;
-  for (int x = 0; x < 16; x++) {
-    for (int y = 0; y < 16; y++) {
-      int rx = test_resistance(x, y);
-      // buf[ptr++] = rx;
-      Serial.write((highByte(rx << 3)) & 31);
-      Serial.write(lowByte(rx & 31));
+  for (x = 0; x < 16; x++) {
+    set_mux_reg(x, reg_x);
+    for (y = 0; y < 16; y++) {
+      set_mux_reg(y, reg_y);
+      sample_delay(out_voltage, delay);
+      Serial.write(out_voltage);
     }
   }
 }
 
-void send() {
+void scan_opt() {
+  uint8_t d = 30;
+  uint8_t x = 0, y = 0;
+  set_mux_reg(x, reg_x);
+  set_mux_reg(y, reg_y);
   Serial.write(0xff);
-  for (int i = 0; i < 256; i++) {
-    int rx = buf[i];
-    Serial.write((highByte(rx << 3)) & 31);
-    Serial.write(lowByte(rx & 31));
+  sample_delay(out_voltage, d);
+
+  for (y = 1; y < 16; y++) {
+    set_mux_reg(y, reg_y);
+    Serial.write(out_voltage);
+    sample_delay(out_voltage, d);
   }
+
+  for (x = 1; x < 16; x++) {
+    set_mux_reg(x, reg_x);
+    for (y = 0; y < 16; y++) {
+      set_mux_reg(y, reg_y);
+      Serial.write(out_voltage);
+      sample_delay(out_voltage, d);
+    }
+  }
+  Serial.write(out_voltage);
 }
 
 inline void set_mux(uint8_t x, uint8_t y) {
-  PORTB &= 0xf0;
-  PORTB += x & 0xf;
-  PORTL &= 0xf0;
-  PORTL += y & 0xf;
+  PORTB = (PORTB & 0xf0) + (x & 0xf);
+  PORTL = (PORTL & 0xf0) + (y & 0xf);
 }
 
-int test_resistance(uint8_t x, uint8_t y) {
-//   digitalWrite(pin_mux_1[0], (x & 1? HIGH : LOW));
-//   digitalWrite(pin_mux_1[1], (x & 2? HIGH : LOW));
-//   digitalWrite(pin_mux_1[2], (x & 4? HIGH : LOW));
-//   digitalWrite(pin_mux_1[3], (x & 8? HIGH : LOW));
-//   digitalWrite(pin_mux_2[0], (y & 1? HIGH : LOW));
-//   digitalWrite(pin_mux_2[1], (y & 2? HIGH : LOW));
-//   digitalWrite(pin_mux_2[2], (y & 4? HIGH : LOW));
-//   digitalWrite(pin_mux_2[3], (y & 8? HIGH : LOW));
-
-  // 调整寄存器的低4位，直接取为x、y的低4位。前提要求引脚必须刚好是某个寄存器的低4位。
-  set_mux(x, y);
-
-//  Serial.println(pin_mux_1_reg);
-//  Serial.println(pin_mux_2_reg);
-  
-  int voltage = analogRead(pin_out_vtg);
-  
-  return voltage;
+void scan_test_digital() {
+  uint8_t x = 0, y = 0;
+  Serial.write(0xff);
+  for (x = 0; x < 16; x++) {
+    digitalWrite(pin_mux_1[0], (x & 1? HIGH : LOW));
+    digitalWrite(pin_mux_1[1], (x & 2? HIGH : LOW));
+    digitalWrite(pin_mux_1[2], (x & 4? HIGH : LOW));
+    digitalWrite(pin_mux_1[3], (x & 8? HIGH : LOW));
+    for (y = 0; y < 16; y++) {
+        digitalWrite(pin_mux_2[0], (y & 1? HIGH : LOW));
+        digitalWrite(pin_mux_2[1], (y & 2? HIGH : LOW));
+        digitalWrite(pin_mux_2[2], (y & 4? HIGH : LOW));
+        digitalWrite(pin_mux_2[3], (y & 8? HIGH : LOW));
+      sample(out_voltage);
+      Serial.write(out_voltage);
+    }
+  }
 }
