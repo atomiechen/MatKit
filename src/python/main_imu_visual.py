e5974bf53454c4cf07e2ca2bc97eda901864ea6b
Atomie CHEN <atomic_cwh@163.com>
2021-06-19 17:32:02 +0800

    Major: use IMU to do AHRS to estimate attitude

--- src/python/main_imu_visual.py
+++ src/python/main_imu_visual.py
@@ -3,10 +3,13 @@ from serial.tools.list_ports import comports
 import argparse
 import numpy as np
 from struct import calcsize, pack, unpack, unpack_from
+import time
 
 from multiprocessing import Process  # 进程
 from multiprocessing import Array  # 共享内存
 from multiprocessing import Value  # 共享内存
+
+import ahrs
 
 from toolkit.visual.player1d import Player1D
 
@@ -21,6 +24,12 @@ try:
 except:
 	pass
 
+RESOLUTION = 2**15
+MAX_ACC = 2 * 9.8  ## m/s^2
+MAX_GYR = 2000  ## degree/s (dps)
+UNIT_ACC = MAX_ACC / RESOLUTION
+UNIT_GYR = MAX_GYR / RESOLUTION
+
 
 def enumerate_ports():
 	# 查看可用端口
@@ -28,21 +37,33 @@ def enumerate_ports():
 	for item in devices_found:
 		print(item)
 
-
-def task_serial(measure, flag, port, baudrate, timeout=None, gyro=False):
+def task_serial(measure, flag, port, baudrate, timeout=None, acc=False, gyr=False):
 	def read_byte():
 		recv = my_serial.read()
 		if len(recv) != 1:
 			raise Exception("Serial timeout!")
 		return recv[0]
 
+	## serial
 	my_serial = Serial(port, baudrate, timeout=timeout)
 	print(my_serial)
 
+	## AHRS
+	madgwick = ahrs.filters.Madgwick()
+	Q = np.array([1., 0., 0., 0.]) # Allocate for quaternions
+
+	## store IMU data
 	data_imu = np.zeros(6, dtype=float)
 	frame_size = 256+12
 
-	## ref: https://blog.csdn.net/weixin_43277501/article/details/104805286
+	## check fps
+	frame_idx = 0
+	last_frame_idx = 0
+	cur_time = time.time()
+	last_time = cur_time
+	last_frame_time = cur_time
+
+	## protocol ref: https://blog.csdn.net/weixin_43277501/article/details/104805286
 	frame = bytearray()
 	begin = False
 	while flag.value != 0:
@@ -65,16 +86,37 @@ def task_serial(measure, flag, port, baudrate, timeout=None, gyro=False):
 					## wrong length, re-fetch a frame
 					print(f"Wrong frame size: {len(frame)}")
 				else:
+					frame_idx += 1
+					last_frame_time = cur_time
+					cur_time = time.time()
+					if cur_time - last_time >= 1:
+						duration = cur_time - last_time
+						frames = frame_idx - last_frame_idx
+						print(f"  frame rate: {frames/duration:.3f} fps")
+						last_time = cur_time
+						last_frame_idx = frame_idx
 					# data_array[:256] = frame[:256]
 					if data_imu is not None:
 						pos = 256
 						for i in range(6):
 							data_imu[i] = unpack_from(f"=h", frame, pos)[0]
 							pos += calcsize(f"=h")
-						if gyro:
-							measure[:3] = data_imu[3:]  ## gyroscope
+						data_imu[:3] *= UNIT_ACC
+						data_imu[3:] *= UNIT_GYR
+						if acc:
+							## accelerometer
+							measure[:3] = data_imu[:3]
+						elif gyr:
+							## gyroscope
+							measure[:3] = data_imu[3:]
 						else:
-							measure[:3] = data_imu[:3]  ## accelerometer
+							## AHRS
+							madgwick.Dt = cur_time - last_frame_time
+							madgwick.frequency = 1 / madgwick.Dt
+							Q = madgwick.updateIMU(Q, gyr=data_imu[3:]*np.pi/180, acc=data_imu[:3])
+							v3 = Q[1:] / Q[0]
+
+							measure[:3] = v3
 				frame = bytearray()
 				begin = False
 			else:
@@ -107,18 +149,20 @@ def main(args):
 
 	if not args.debug:
 		p = Process(target=task_serial, args=(measure, flag, args.port, 
-									args.baudrate, args.timeout, args.gyro))
+									args.baudrate, args.timeout, args.acc, args.gyr))
 	else:
 		p = Process(target=task_debug, args=(measure,flag,))
 	p.start()
 
-	if args.gyro:
-		ytop = 2000
-		ybottom = -2000
-	else:
-		ytop = None
-		ybottom = None
-	my_player = Player1D(generator=gen_wrapper(), channels=3, timespan=10, 
+	ytop = None
+	ybottom = None
+	if args.acc:
+		ytop = MAX_ACC
+		ybottom = -MAX_ACC
+	elif args.gyr:
+		ytop = MAX_GYR
+		ybottom = -MAX_GYR
+	my_player = Player1D(generator=gen_wrapper(), channels=3, timespan=5, 
 						ytop=ytop, ybottom=ybottom)
 	my_player.run_stream()
 
@@ -133,7 +177,8 @@ if __name__ == '__main__':
 	parser.add_argument('-b', dest='baudrate', action=('store'), default=BAUDRATE, type=int, help="specify baudrate")
 	parser.add_argument('-t', dest='timeout', action=('store'), default=TIMEOUT, type=float, help="specify timeout in seconds")
 	parser.add_argument('-d', '--debug', dest='debug', action=('store_true'), default=False, help="debug mode")
-	parser.add_argument('-g', '--gyro', dest='gyro', action=('store_true'), default=False, help="show gyroscope data (otherwise accelerometer)")
+	parser.add_argument('-g', '--gyr', dest='gyr', action=('store_true'), default=False, help="show gyroscope data")
+	parser.add_argument('-a', '--acc', dest='acc', action=('store_true'), default=False, help="show accelerometer data")
 
 	args = parser.parse_args()
 
