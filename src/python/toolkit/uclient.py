8c8aa3c6f14c456b939bbc32e26d3080f0ec5428
Atomie CHEN <atomic_cwh@163.com>
2021-07-05 11:54:52 +0800

    Minor: combine userver & uclient's CMD definition

--- src/python/toolkit/uclient.py
+++ src/python/toolkit/uclient.py
@@ -12,7 +12,6 @@ try:
 	support_unix_socket = True
 except ImportError:
 	support_unix_socket = False
-from enum import IntEnum
 from random import randint
 from numpy import zeros
 from os import unlink
@@ -21,33 +20,7 @@ from struct import calcsize, pack, unpack, unpack_from
 from typing import Iterable
 
 from .tools import check_shape
-
-
-class CMD(IntEnum):
-
-	"""Pre-defined commands for communication with server.
-	
-	Attributes:
-		CLOSE (int): Close the server
-		DATA (int): get processed data frame and frame index
-		RAW (int): get raw data frame and frame index
-		REC_DATA (int): record processed data to file
-		REC_RAW (int): record raw data to file
-		REC_STOP (int): stop recording
-		RESTART (int): restart the server with processing parameters
-		PARAS (int): get current processing parameters of the server
-		REC_BREAK (int): stop current recording and start a new one
-	"""
-	
-	CLOSE = 0
-	DATA = 1
-	RAW = 2
-	REC_DATA = 3
-	REC_RAW = 4
-	REC_STOP = 5
-	RESTART = 6
-	PARAS = 7
-	REC_BREAK = 8
+from .cmd import CMD
 
 
 class Uclient:
