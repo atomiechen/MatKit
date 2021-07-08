9597237e46b0144c822893c4fcea63b775a03952
Atomie CHEN <atomic_cwh@163.com>
2021-07-09 00:22:47 +0800

    Major: swipe gesture support CLICK; update gesture test case

---
+++ src/python/demo_gesture_remote.py
@@ -0,0 +1,104 @@
+import argparse
+
+from toolkit.uclient import Uclient
+from toolkit.process import Processor, CursorController
+from toolkit.tools import load_config, blank_config
+
+from demokit.swipe_gesture import SwipeGesture, SwipeGestureClassifier
+from demokit.cursor_client import CursorClient
+
+
+CONFIG_PATH = "config/tmp_config_mouth_16_16.yaml"
+LEFT = 0.1
+RIGHT = 0.9
+UP = 0.1
+DOWN = 0.9
+
+IP = "localhost"
+PORT = 8081
+
+
+def mapping(x, y, left=0, right=1, up=0, down=1):
+	x0 = 1-y
+	y0 = 1-x
+	x1 = min(max(x0 - left, 0) / (right - left), 0.999)
+	y1 = min(max(y0 - up, 0) / (down - up), 0.999)
+	return x1, y1
+
+def main():
+	## load config and combine commandline arguments
+	if args.config:
+		config = load_config(args.config)
+	else:
+		config = blank_config()
+
+	my_processor = Processor(
+		config['process']['interp'], 
+		blob=config['process']['blob'], 
+		threshold=config['process']['threshold'],
+		order=config['process']['interp_order'],
+		total=config['process']['blob_num'],
+		special=config['process']['special_check'],
+	)
+	my_processor.print_info()
+	my_cursor = CursorController(
+		mapcoor=config['pointing']['direct_map'],
+		alpha=config['pointing']['alpha'], 
+		trackpoint=config['pointing']['trackpoint']
+	)
+	my_cursor.print_info()
+
+	with CursorClient(IP, PORT) as my_remote_handle:
+		with Uclient(
+			config['connection']['client_address'], 
+			config['connection']['server_address'], 
+			udp=config['connection']['udp'], 
+			n=config['sensor']['shape']
+		) as my_client:
+			my_classifier = SwipeGestureClassifier(debug=args.debug)
+			frame_idx = 0
+			while True:
+				## get a new frame
+				frame, frame_idx = my_client.fetch_frame_and_index(new=True)
+
+				row, col, val = my_processor.parse(frame)
+				moving, x, y, val = my_cursor.update(row, col, val)
+				x, y = mapping(x, y, left=LEFT, right=RIGHT, up=UP, down=DOWN)
+
+				gesture = my_classifier.classify(x, y, moving)
+				if gesture != SwipeGesture.NONE:
+					print(f"gesture: {gesture.name}")
+
+				if args.morse:
+					if gesture == SwipeGesture.UP:
+						my_remote_handle.sendButton('di')
+					elif gesture == SwipeGesture.DOWN:
+						my_remote_handle.sendButton('da')
+					elif gesture == SwipeGesture.LEFT:
+						my_remote_handle.sendButton('reset')
+					elif gesture == SwipeGesture.RIGHT:
+						my_remote_handle.sendButton('reset')
+					elif gesture == SwipeGesture.CLICK:
+						my_remote_handle.sendButton('space')
+				else:
+					if gesture == SwipeGesture.UP:
+						my_remote_handle.sendButton('up')
+					elif gesture == SwipeGesture.DOWN:
+						my_remote_handle.sendButton('down')
+					elif gesture == SwipeGesture.LEFT:
+						my_remote_handle.sendButton('left')
+					elif gesture == SwipeGesture.RIGHT:
+						my_remote_handle.sendButton('right')
+					elif gesture == SwipeGesture.CLICK:
+						my_remote_handle.sendButton('click')
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser.add_argument('--config', dest='config', action='store', default=CONFIG_PATH, help="specify configuration file")
+	parser.add_argument('-m', '--morse', dest='morse', action='store_true', default=False, help="Morse input")
+
+	parser.add_argument('--debug', dest='debug', action='store_true', default=False, help="debug mode")
+	args = parser.parse_args()
+
+	main()
--- src/python/demokit/swipe_gesture.py
+++ src/python/demokit/swipe_gesture.py
@@ -1,4 +1,6 @@
 from enum import Enum, IntEnum
+import time
+
 import numpy as np
 import scipy.interpolate
 
@@ -10,17 +12,21 @@ class SwipeGesture(IntEnum):
 	RIGHT = 2
 	UP = 3
 	DOWN = 4
+	CLICK = 5
 
 class SwipeGestureClassifier:
 
 	INTERP_NUM = 40
-	THRESHOLD = 5
+	TH_SWIPE = 3
+	TH_CLICK = 0.05
+	START_BORDER = 0.5
 
 	def __init__(self, debug=False):
-		# self.gesture = SwipeGesture.NONE
 		self.in_gesture = False
 		self.point_x = []
 		self.point_y = []
+		self.start_time = time.time()
+		self.duration = 0
 
 		self.template_hori = np.zeros((2, self.INTERP_NUM))
 		self.template_hori[0] = np.linspace(0, 1, self.INTERP_NUM)
@@ -31,6 +37,13 @@ class SwipeGestureClassifier:
 
 	def check(self):
 		ret = SwipeGesture.NONE
+
+		x_dis = self.point_x[-1] - self.point_x[0]
+		y_dis = self.point_y[-1] - self.point_y[0]
+		displacement = (x_dis**2+y_dis**2)**0.5
+		if displacement <= self.TH_CLICK:
+			ret = SwipeGesture.CLICK
+			return ret
 
 		## !!! Note: splprep cannot handle < successive identical points >
 		## ref: https://stackoverflow.com/a/61859042/11854304
@@ -50,17 +63,23 @@ class SwipeGestureClassifier:
 		y_range = new_points[1].max() - new_points[1].min()
 		if x_range > y_range:
 			## horizontal
-			new_points[0] -= new_points[0].min()
-			new_points *= 1 / x_range
+
+			## reverse if necessary
 			if new_points[0][-1] - new_points[0][0] < 0:
 				new_points = np.flip(new_points, axis=1)
 				reverse = True
+			## start point is not in valid region
+			if new_points[0][0] >= self.START_BORDER:
+				return ret
+
+			new_points[0] -= new_points[0].min()
+			new_points *= 1 / x_range
 			## compare with template
 			diff = new_points - self.template_hori
 			sim = np.linalg.norm(diff, axis=0).mean()
 			if self.debug:
 				print(f"horizontal sim: {sim}  reverse: {reverse}")
-			if sim <= self.THRESHOLD:
+			if sim <= self.TH_SWIPE:
 				if reverse:
 					ret = SwipeGesture.LEFT
 				else:
@@ -68,17 +87,23 @@ class SwipeGestureClassifier:
 
 		else:
 			## vertical
-			new_points[1] -= new_points[1].min()
-			new_points *= 1 / y_range
+
+			## reverse if necessary
 			if new_points[1][-1] - new_points[1][0] < 0:
 				new_points = np.flip(new_points, axis=1)
 				reverse = True
+			## start point is not in valid region
+			if new_points[1][0] >= self.START_BORDER:
+				return ret
+
+			new_points[1] -= new_points[1].min()
+			new_points *= 1 / y_range
 			## compare with template
 			diff = new_points - self.template_vert
 			sim = np.linalg.norm(diff, axis=0).mean()
 			if self.debug:
 				print(f"vertical sim: {sim}  reverse: {reverse}")
-			if sim <= self.THRESHOLD:
+			if sim <= self.TH_SWIPE:
 				if reverse:
 					ret = SwipeGesture.UP
 				else:
@@ -101,12 +126,15 @@ class SwipeGestureClassifier:
 	def classify(self, x, y, moving):
 		gesture = SwipeGesture.NONE
 		if moving:
+			if len(self.point_x) == 0:
+				self.start_time = time.time()
 			if len(self.point_x) == 0 or self.point_x[-1] != x or self.point_y[-1] != y:
 				self.point_x.append(x)
 				self.point_y.append(y)
 			self.in_gesture = True
 		else:
 			if self.in_gesture:
+				self.duration = time.time() - self.start_time
 				gesture = self.check()
 				self.point_x = []
 				self.point_y = []
