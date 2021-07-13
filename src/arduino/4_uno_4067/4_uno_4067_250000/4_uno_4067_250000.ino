34661513165d55d19ce01be77ba4d4cbea7a2b2c
dBHz01 <928300456@qq.com>
2021-07-13 14:27:31 +0800

    as last commit add arduino code for different baud rate

---
+++ src/arduino/4_uno_4067/4_uno_4067_250000/4_uno_4067_250000.ino
@@ -0,0 +1,33 @@
+bool digital_S0[] = {0, 1, 0, 1};
+bool digital_S1[] = {0, 0, 1, 1};
+int vol = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(250000);
+  for (int i = 2; i <= 11; i++){
+    pinMode(i, OUTPUT);
+  }
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.write(0xFF);
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(3, digital_S0[i]);
+    digitalWrite(2, digital_S1[i]);
+    for (int j = 0; j < 4; j++) {
+      digitalWrite(8, digital_S0[j]);
+      digitalWrite(7, digital_S1[j]);
+      vol = analogRead(A0);
+      int out = (vol >> 2);
+      Serial.write(out);
+    }
+  }
+}
